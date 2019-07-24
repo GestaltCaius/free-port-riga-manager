459e85134ea458a5776afab2509afb9f38c69c15
Student <Student@ITD-NB.RBP.lv>
2019-07-24 15:01:47 +0300

    add AuthGuard draft

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from './user';
+import { LoginResponse } from './login-response';
 
 @Injectable({
   providedIn: 'root'
@@ -10,22 +11,33 @@ export class AuthService {
 
   private BASE_URL: string = 'http://localhost:5000/auth';
 
-  private httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type':  'application/json',
-      'Authorization': 'my-auth-token' // Seulement quand on use le JWT
-    })
-  };
 
   constructor(private http: HttpClient) { }
 
-  test(): string {
-    return 'working';
-  }
+  login(user: User): void {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+      })
+    };
 
-  login(user: User): Observable<any> {
     let url: string = `${this.BASE_URL}/login`;
-    return this.http.post(url, user, this.httpOptions);
+    this.http.post<LoginResponse>(url, user, httpOptions).subscribe(
+      loginResponse => {
+        localStorage.setItem('jwt_access_token', loginResponse.access_token);
+        localStorage.setItem('jwt_refresh_token', loginResponse.refresh_token);
+      },
+      err => console.log('error') // TODO
+    );
   }
 
+  isLoggedIn(): Observable<string> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': localStorage.getItem('jwt_access_token'),
+      })
+    };
+    return this.http.get<string>('${this.BASE_URL}/isloggedin', httpOptions)
+  }
 }
